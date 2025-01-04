9db854f2b331613038da5a13927e8cf9c0f81cf1
Gautierpicon <gautierpicon@proton.me>
2025-01-04 14:07:56 +0100

    Remove unwanted files and update .gitignore

--- src/types/todo.ts
+++
@@ -1,9 +0,0 @@
-export type TodoStatus = 'todo' | 'inProgress' | 'done' | 'frozen';
-
-export interface Todo {
-  id: string;
-  title: string;
-  status: TodoStatus;
-  createdAt: Date;
-  previousStatus: TodoStatus;
-}
